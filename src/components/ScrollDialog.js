2bc51b788e5543cec08cc14aca75be9614a1ea08
AnnaH <66872647+annahasselstrom@users.noreply.github.com>
2021-04-24 17:47:22 +0200

    Update ScrollDialog.js

--- src/components/ScrollDialog.js
+++ src/components/ScrollDialog.js
@@ -4,7 +4,6 @@ import { Tooltip } from '@material-ui/core';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import { Forecast } from './Forecast';
 import PropTypes from 'prop-types';
 
